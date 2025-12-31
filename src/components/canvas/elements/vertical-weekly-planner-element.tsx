31fe1d5bc227abd8d9392e68d4c211e17f817af2
Pia Finlay <pia@gmail.com>
2025-12-31 19:24:06 -0300

    style: Cambiar borde del contenedor principal a segmentado negro (border-dashed border-black)

--- src/components/canvas/elements/vertical-weekly-planner-element.tsx
+++ src/components/canvas/elements/vertical-weekly-planner-element.tsx
@@ -115,7 +115,7 @@ export default function VerticalWeeklyPlannerElement(props: CommonElementProps) 
     <div
       data-element-id={id}
         className={cn(
-          'w-full h-full flex flex-col overflow-hidden rounded-2xl border border-black shadow-md',
+          'w-full h-full flex flex-col overflow-hidden rounded-2xl border-2 border-dashed border-black shadow-md',
           isSelected && 'ring-2 ring-primary/40'
         )}
       style={{
